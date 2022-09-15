a66177080ba2ac6547cc50c6daeeddee3aff2215
kalz2q <katsu95i@gmail.com>
2022-09-16 08:59:48 +0900

    MM

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,15 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="ear"></div>
-        {/* <div className="ear" style="--right: 0"></div> */}
         <div className="ear" ></div>
         <div className="face">
-          {/* <div className="eye" style="--left: 20%"></div> */}
           <div className="eye"></div>
-
-          {/* <div className="eye" style="--right: 20%"></div> */}
           <div className="around-mouth"></div>
           <div className="nose">
             <div className="mouth"></div>
           </div>
-          {/* <div className="beard" style="--left: 15%"></div> */}
-          {/* <div className="beard" style="--right: 15%; --rotate: -1"></div> */}
           <div className="top-pattern"></div>
           <div className="side-pattern"></div>
-          {/* <div className="side-pattern" style="--right: 0; --rotate: -1"></div> */}
         </div>
       </div>
     </div>
